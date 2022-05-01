118642d2bc9da1193abda098ee84492615536777
Olumide Akinsola <olumideakinsola@XMac.local>
2022-04-30 20:19:08 -0400

    Commit test

---
+++ main.py
@@ -0,0 +1,14 @@
+# lambda_count = lambda a : a + 10
+# d = lambda_count(6)
+# print(type(lambda_count))
+#
+# print(d)
+
+# d = {"shoes": "shoes", "clothes": "clothes"}
+# print(d)
+# print("\r")
+#
+# d["claim_type", "Country"] = "Institutional", "Canada"
+# print(d)
+# print("\r")
+# print(d['Country'])
