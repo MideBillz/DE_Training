00fd9cd770715ec5ef83fb858c130c3427ea813d
Olumide Akinsola <olumideakinsola@XMac.local>
2022-05-09 12:06:40 -0400

    reviewed notes file

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-                            ##############TO BE REVIEWED###########
-
-# lambda_count = lambda a : a + 10
-# d = lambda_count(6)
-# print(type(lambda_count))
-#
-# print(d)
-
-# d = {"shoes": "shoes", "clothes": "clothes"}
-# print(d)
-# print("\r")
-# d["claim_type", "Country"] = "Institutional", "Canada"
-# print(type(d))
-# print("\r")
-# print(d["Country"])
+print('Hello World')
