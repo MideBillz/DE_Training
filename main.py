1ea1ea1c1ad59ce5eedd0cea428a13011968c005
Olumide Akinsola <olumideakinsola@XMac.local>
2022-06-14 15:21:14 -0400

    modified idea/vsc.xml file to remove root error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import ingest
 import persist
+import logging
 
+print("This is a program")
 
 class Test:
     def __init__(self, fileType):
@@ -15,9 +17,12 @@ class Test:
 
 
 if __name__ == '__main__':
+    print("Entering the main method")
+    logging.basicConfig(level = "DEBUG")
+    logging.debug("This is a debug message")
+    logging.info("This is an info message")
+    logging.warning("This is a warning message")
+    logging.error("This is an error message")
+    print("Hello World")
     eg = Test("csv")  #Creating an instance of the objects
     eg.my_function()
-    # reader = ingest.FileReader()
-    # writer = persist.PersistData()
-    # reader.read_file()
-    # writer.store_data()
